d1b5777f1c57d19317c31130768eb282d627b555
clairehousez <97857341+clairehousez@users.noreply.github.com>
2022-05-03 18:00:32 -0400

    the error has been resolved

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -1,19 +1,24 @@
 
 function TeamInfo(props) {
-    console.log(props.finalTeam)
-
     return (
-        <div className="teamInfo">
-            <h2>{props.finalTeam.name}</h2>
+        <div>
+            {
+            !props.finalTeam.name ? <h2>Select a team!</h2> :
+
+            <div className="teamInfo">
+
+                <h2>{props.finalTeam.name}</h2>
             
-            <h4>{props.finalTeam.abbreviation}</h4>
-            <p>Location: {props.finalTeam.locationName}</p>
-            <p>First Year: {props.finalTeam.firstYearOfPlay}</p>
-            <p>Website: {props.finalTeam.officialSiteUrl}</p>
-            <p>Conference: {props.finalTeam.conference.name}</p>
+                <h4>{props.finalTeam.abbreviation}</h4>
+                <p>Location: {props.finalTeam.locationName}</p>
+                <p>Conference: {props.finalTeam.conference.name}</p>
+                <p>First Year: {props.finalTeam.firstYearOfPlay}</p>
+                <p>Website: {props.finalTeam.officialSiteUrl}</p>
             
             
             
+            </div>
+            }
         </div>
     )
 }
